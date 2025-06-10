806e61179fa00fb737be5dc885ea6aaa4b9cc197
Murilo <falconeri1501@gmail.com>
2025-06-09 21:53:46 -0300

    add comon middlewa

--- routes/api.php
+++ routes/api.php
@@ -4,10 +4,10 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\CommonUserController;
 use App\Http\Controllers\AuthController;
-use App\Http\Middleware\AdminMiddleware;
+use App\Http\Middleware\CommonMiddleware;
 
 //common
-Route::get('common/user/{id}',[CommonUserController::class,'index']);
+Route::get('common/user/{id}',[CommonUserController::class,'index']) ->middleware(['auth:sanctum', CommonMiddleware::class]);
 Route::post('common/user',[CommonUserController::class,'store']);
 
 //auth
