8a9fc0118dfe9ef240a18ae666f44ae8ecce1ed1
Murilo <falconeri1501@gmail.com>
2025-06-10 21:32:47 -0300

    inserting admin user

--- app/Http/Controllers/AdminUserController.php
+++ app/Http/Controllers/AdminUserController.php
@@ -2,9 +2,36 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\UserModel;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use App\Http\Requests\ValidationPostRequest;
 
 class AdminUserController extends Controller
 {
-    //
+    public function index()
+    {
+
+    }
+
+    public function store(ValidationPostRequest $request)
+    {
+        $request['password'] = Hash::make($request['password']);
+
+        $user = UserModel::create($request->all());
+
+        return response()->json([
+            "message" => "admin user registered succesfully"
+        ],201);
+    }
+
+    public function update()
+    {
+
+    }
+
+    public function destroy()
+    {
+
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -2,16 +2,22 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\CommonUserController;
-use App\Http\Controllers\AuthController;
 use App\Http\Middleware\RoleMiddleware;
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\AdminUserController;
+use App\Http\Controllers\CommonUserController;
 
 //common
 Route::middleware(['auth:sanctum', RoleMiddleware::class.':comon'])->group(function () {
     Route::get('common/user/{id}',[CommonUserController::class,'index']);
 });
-
 Route::post('common/user',[CommonUserController::class,'store']);
+
+//admin
+Route::middleware(['auth:sanctum', RoleMiddleware::class.':admin'])->group(function () {
+    Route::post('admin/user',[AdminUserController::class,'store']);
+});
+
 
 //auth
 Route::post('auth/login', [AuthController::class, 'login']);
